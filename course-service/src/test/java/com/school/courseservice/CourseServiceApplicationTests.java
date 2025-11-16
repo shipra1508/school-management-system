c5a6792ad3845defdfd2b7e0bdf506006125ac4d
Shipra <shiprashikta@gmail.com>
2025-11-16 14:44:29 -0600

    initial setup for course-service with PostgreSQL and logging configuration

---
+++ course-service/src/test/java/com/school/courseservice/CourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.courseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
